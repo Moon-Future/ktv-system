709909de2949da81f7220c48dd3fe13396425402
ChenLiang <236338364@qq.com>
2019-01-14 18:25:23 +0800

    reportForm

---
+++ src/views/admin/reportForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="report-container">
+
+  </div>
+</template>
+
+<script>
+  import BaseTable from '@/components/admin/BaseTable'
+  import { dateFormat } from '@/common/js/util'
+  import { apiUrl } from '@/serviceAPI.config.js'
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
